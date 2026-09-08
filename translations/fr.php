edc251b5e26c6abf4cab6ceb2a35e4a6716c3e79
3dpluspub <3dpluspub@gmail.com>
2026-09-08 12:46:45 +0200

    Add: French translations

---
+++ translations/fr.php
@@ -0,0 +1,23 @@
+<?php
+
+global $_MODULE;
+$_MODULE = [];
+
+$_MODULE['<{mincomv3}prestashop>mincomv3_1234567890'] = 'MinComV3';
+$_MODULE['<{mincomv3}prestashop>mincomv3_displayname'] = 'MinComV3 - Frais et Minimum par Ville';
+$_MODULE['<{mincomv3}prestashop>mincomv3_description'] = 'Gestion des frais de port et minimum de commande par ville et code postal.';
+$_MODULE['<{mincomv3}prestashop>mincomv3_city_management'] = 'Gestion des Villes';
+$_MODULE['<{mincomv3}prestashop>mincomv3_postal_code'] = 'Code Postal';
+$_MODULE['<{mincomv3}prestashop>mincomv3_city_name'] = 'Ville';
+$_MODULE['<{mincomv3}prestashop>mincomv3_shipping_cost'] = 'Frais de Port';
+$_MODULE['<{mincomv3}prestashop>mincomv3_free_shipping_amount'] = 'Montant Franco (Minimum)';
+$_MODULE['<{mincomv3}prestashop>mincomv3_active'] = 'Actif';
+$_MODULE['<{mincomv3}prestashop>mincomv3_insufficient_amount'] = 'Montant insuffisant ! Le minimum requis pour cette zone est de %.2f CHF. (Panier actuel : %.2f CHF)';
+$_MODULE['<{mincomv3}prestashop>mincomv3_order_refused'] = 'COMMANDE REFUSÉE : Le minimum requis pour cette zone est de %.2f CHF (Panier: %.2f CHF).';
+$_MODULE['<{mincomv3}prestashop>mincomv3_local_delivery'] = 'Livraison Locale';
+$_MODULE['<{mincomv3}prestashop>mincomv3_fast_delivery'] = 'Livraison rapide à domicile';
+$_MODULE['<{mincomv3}prestashop>mincomv3_confirm_delete'] = 'Êtes-vous sûr ?';
+$_MODULE['<{mincomv3}prestashop>mincomv3_delete_selected'] = 'Supprimer les éléments sélectionnés';
+$_MODULE['<{mincomv3}prestashop>mincomv3_save'] = 'Enregistrer';
+$_MODULE['<{mincomv3}prestashop>mincomv3_yes'] = 'Oui';
+$_MODULE['<{mincomv3}prestashop>mincomv3_no'] = 'Non';
